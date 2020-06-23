2aa0d29204aee697bb371a7eea2a9355ad1a9f69
reactive-source-2578 <mimioj@gmail.com>
2020-06-23 16:50:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
+  puts "Welcome to Tic Tac Toe
+   |   |   
+-----------
+   |   |   
+-----------
+   |   |   "
+   
+ end
+ 
+ display_board
